61500fe58ff8a3948a3d26169fb7df793e7236ea
Danylo Mysak <danmysak@gmail.com>
2019-09-26 15:49:55 +0300

    Initial commit

---
+++ src/invertObject.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Implement a function that inverts an object:
+ *
+ * Every `key: value` pair of the object passed as the only argument to the
+ * function must become `value: key` in the object that you return. If there are
+ * more than one property with the same value in the original object,
+ * return `null` instead.
+ *
+ *
+ * invertObject({}) is {}
+ * invertObject({"foo": "bar"}) is {"bar": "foo"}
+ * invertObject({"1": 2, "2": 4, "3": 6}) is {"2": "1", "4": "2", "6": "3"}
+ * invertObject({"foo": "bar", "hello": "world", "js": "bar"}) === null
+ *
+ * @param {Object} items
+ *
+ * @return {Object}
+ */
+function invertObject(items) {
+  // write code here
+}
+
+module.exports = invertObject;
---
+++ src/invertObject.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const invertObject = require('./invertObject');
+
+test('Empty object', () => {
+  expect(invertObject({}))
+    .toEqual({});
+});
+
+test('Simple object with one property', () => {
+  expect(invertObject({ 'foo': 'bar' }))
+    .toEqual({ 'bar': 'foo' });
+});
+
+test('Numeric keys and values', () => {
+  expect(invertObject({ '1': 2, '2': 4, '3': 6 }))
+    .toEqual({ '2': '1', '4': '2', '6': '3' });
+});
+
+test('Repeating values', () => {
+  expect(invertObject({ 'foo': 'bar', 'hello': 'world', 'js': 'bar' }))
+    .toEqual(null);
+});
